245244fe6ec14ad6397e0fdbd0609b3d9125ba95
dejene tesfaye <dejenet@tnt.com>
2025-03-24 13:22:13 +0300

    setup the signin,signup,and dashboard in client side

--- server/inventory/models.py
+++ server/inventory/models.py
@@ -26,10 +26,10 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    username = None # Keep it optional
+    username = None 
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []  # No username required
+    REQUIRED_FIELDS = []  
 
     objects = UserManager()  # Use the custom manager
 
